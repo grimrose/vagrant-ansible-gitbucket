354cf30d281a8cb7d4d0c643d4dadd9e2b9211a8
grimrose <grwin7@gmail.com>
2014-03-07 02:44:52 +0900

    fix jetty role

--- provision.sh
+++ provision.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+if ! [ `which add-apt-repository` ]; then
+	apt-get install -y python-software-properties
+	apt-get update -y
+	apt-add-repository -y ppa:rquillo/ansible
+fi
+
 if ! [ `which ansible` ]; then
-    apt-get update -y
-    apt-get install -y ansible
+	apt-get update -y
+	apt-get install -y ansible
 fi
 
 ansible-playbook -i /vagrant/ansible/hosts /vagrant/ansible/site.yml

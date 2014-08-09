c2212886e4fe09475f4bc3284828215b31a60693
grimrose <grwin7@gmail.com>
2014-08-10 03:10:50 +0900

    Update to using local ansible

--- Vagrantfile
+++ Vagrantfile
@@ -9,6 +9,5 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "precise32"
   config.vm.network :private_network, ip: "192.168.33.10"
   config.vm.synced_folder ".", "/vagrant", :mount_options => ['dmode=775', 'fmode=664']
-  config.vm.provision "shell", :path => "provision.sh"
 
 end
--- provision.sh
+++ provision.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 
-if ! [ `which add-apt-repository` ]; then
-	apt-get install -y python-software-properties
-	apt-get update -y
-	apt-add-repository -y ppa:rquillo/ansible
-fi
-
-if ! [ `which ansible` ]; then
-	apt-get update -y
-	apt-get install -y ansible
-fi
-
-ansible-playbook -i /vagrant/ansible/hosts /vagrant/ansible/site.yml
+ansible-playbook -i ansible/hosts ansible/site.yml --private-key=~/.vagrant.d/insecure_private_key -v
